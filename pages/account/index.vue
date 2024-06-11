8887c7085e702b2902eb103a00de3e26491a2ae1
Sam K <33263129+samk-dev@users.noreply.github.com>
2024-06-11 18:36:12 +0200

    Feat/oauth spotify (#7)
    * feat: spotify login + uploads
    
    * fix: debug - cloudflare execede cpu
    
    * fix: debug cloudflare excede cpu
    
    * fix: debug - removed uploade route

--- pages/account/index.vue
+++ pages/account/index.vue
@@ -2,7 +2,7 @@
 const user = useUser()
 const { logout } = useAuth()
 const toast = useToast()
-const { $appwrite } = useNuxtApp()
+// const { $appwrite } = useNuxtApp()
 
 const loading = ref(false)
 
@@ -19,36 +19,36 @@ const onLogout = async () => {
 }
 
 // upload a file
-const fileInput = ref<File | null>(null)
-const handleFileChange = (event: Event) => {
-  const target = event.target as HTMLInputElement
-  if (target.files && target.files[0]) {
-    fileInput.value = target.files[0]
-  }
-}
+// const fileInput = ref<File | null>(null)
+// const handleFileChange = (event: Event) => {
+//   const target = event.target as HTMLInputElement
+//   if (target.files && target.files[0]) {
+//     fileInput.value = target.files[0]
+//   }
+// }
 
-const uploadFile = async () => {
-  if (!fileInput.value) {
-    alert('Please select a file to upload')
-    return
-  }
+// const uploadFile = async () => {
+//   if (!fileInput.value) {
+//     alert('Please select a file to upload')
+//     return
+//   }
 
-  const formData = new FormData()
-  formData.append('file', fileInput.value)
-  console.log('FILE', ...formData.entries())
-  try {
-    const response = await $appwrite('/api/v1/upload', {
-      method: 'POST',
-      body: formData
-    })
+//   const formData = new FormData()
+//   formData.append('file', fileInput.value)
+//   console.log('FILE', ...formData.entries())
+//   try {
+//     const response = await $appwrite('/api/v1/upload', {
+//       method: 'POST',
+//       body: formData
+//     })
 
-    alert('File uploaded successfully')
-    console.log(response)
-  } catch (error) {
-    alert('Error uploading file')
-    console.error(error)
-  }
-}
+//     alert('File uploaded successfully')
+//     console.log(response)
+//   } catch (error) {
+//     alert('Error uploading file')
+//     console.error(error)
+//   }
+// }
 </script>
 
 <template>
@@ -56,10 +56,10 @@ const uploadFile = async () => {
     account
     <h1>hi {{ user?.name }}</h1>
 
-    <form @submit.prevent="uploadFile">
+    <!-- <form @submit.prevent="uploadFile">
       <input ref="fileInput" type="file" @change="handleFileChange" />
       <button type="submit">Upload</button>
-    </form>
+    </form> -->
 
     <UButton :loading :disabled="loading" @click="onLogout">Logout</UButton>
   </div>
